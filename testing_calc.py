07d043c52361ade05de068bc187eb87f2ab3e930
joshua121 <joshua121@schule.bremen.de>
2020-01-21 13:43:36 +0100

    plicht: -size -weight
    optinal
    -age
    -sex

--- testing_calc.py
+++ testing_calc.py
@@ -1,32 +1,40 @@
 class BMIclac:
-    def __init__(self, bmi):
+    def __init__(self, bmi, sex):
         self.bmi = bmi
-        self.sex = "male"
-        self.BMItable = {"no_sex": ("FEHLER", ("Untergewicht", 0, 18.4), ("Normalgewicht", 18.5, 24.9),
-                                    ("Übergewicht", 25, 29.9), ("Starkes Übergewicht(Apdipositas Grad I)", 30, 34.9),
-                                    ("Apdipositas Grad II", 35, 39.9),("Adipositas Grad III", 40, 0)),
-                         "male": ("FEHLER", ("Untergewicht", 0, 20), ("Normalgewicht", 20, 24,9),
-                                  ("Übergewicht", 25, 29.9), ("Starkes Übergewicht(Apdipositas Grad I)", 30, 34.9),
-                                  ("Apdipositas Grad II", 35, 39.9), ("Adipositas Grad III", 40, 0)),
-                         "female": ("FEHLER", ("Untergewicht", 0, 19), ("Normalgewicht", 19, 23,9),
-                                    ("Übergewicht", 24, 29.9),
-                                    ("Starkes Übergewicht(Apdipositas Grad I)", 30, 34.9),
-                                    ("Apdipositas Grad II", 35, 39.9), ("Adipositas Grad III", 40, 0))}
+        self.sex = sex
+
+        self.BMItable = {"no_sex": ("FEHLER", ("Untergewicht", 0.0, 18.4), ("Normalgewicht", 18.5, 24.9),
+                                    ("Übergewicht", 25.0, 29.9),
+                                    ("Starkes Übergewicht(Apdipositas Grad I)", 30.0, 34.9),
+                                    ("Apdipositas Grad II", 35.0, 39.9),("Adipositas Grad III", 40.0, 1000000000000.0)),
+                         "male": ("FEHLER", ("Untergewicht", 0.0, 20.0), ("Normalgewicht", 20.0, 24.9),
+                                  ("Übergewicht", 25.0, 29.9), ("Starkes Übergewicht(Apdipositas Grad I)", 30.0, 34.9),
+                                  ("Apdipositas Grad II", 35.0, 39.9), ("Adipositas Grad III", 40.0, 10000000000000.0)),
+                         "female": ("FEHLER", ("Untergewicht", 0.0, 19.0), ("Normalgewicht", 19.0, 23.9),
+                                    ("Übergewicht", 24.0, 29.9),
+                                    ("Starkes Übergewicht(Apdipositas Grad I)", 30.0, 34.9),
+                                    ("Apdipositas Grad II", 35.0, 39.9), ("Adipositas Grad III", 40.0, 100000000000.0))}
 
     def set_result(self):
         counter = 1
         result = 0
-        if self.sex == "male": bmitable = self.BMItable["male"]
-        if self.sex == "female": bmitable = self.BMItable["female"]
-        if self.sex == "no_sex": bmitable = self.BMItable["no_sex"]
+        bmitable = self.BMItable["no_sex"]
+        if self.sex == "male":
+            bmitable = self.BMItable["male"]
+        if self.sex == "female":
+            bmitable = self.BMItable["female"]
         for element in bmitable[1:]:
-            counter = counter + 1
             if self.bmi > element[1] < element[2]:
                 result = counter
+            counter = counter + 1
         print(bmitable[result][0])
 
+    def set_bmi(self):
+        pass
 
-bmi = float(input("BMI:"))
-BMI = BMIclac(bmi)
+
+bmi = float(input("BMI: "))
+sex = str(input("Sex: "))
+BMI = BMIclac(bmi, sex)
 
 BMI.set_result()
