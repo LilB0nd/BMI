8fc1fae5882fa357a1275e1f7316d50eb3cba4f4
DatFlash <anil30@schule.bremen.de>
2020-01-21 11:28:06 +0100

    Ausgaben nicht fertig

--- BMI_programm.py
+++ BMI_programm.py
@@ -1,8 +1,8 @@
 from noname import Panel
 import wx
+from typing import Optional
 
-
-class BMIcalc(Panel):
+class BmiCalc:
     """Interface for BMI Calculation"""
 
     def get_bmi(self) -> float:
@@ -19,54 +19,49 @@ class BMIcalc(Panel):
         """ get current age
         :return: current age
         """
+        pass  # TODO
 
-
-    def set_age(self, age: int) -> None:
-        """ Set new age
-        :param age: new age
+    def set_age(self, age: Optional[int]) -> None:
+        """ Set new or reset age
+        :param age: new age or None to reset
         """
-        self.age = age
-        return None
+        pass  # TODO
 
     def get_sex(self) -> str:
         """ get current sex as 'm' or 'f'
         :return: current sex
         """
-        return sex
+        pass  # TODO
 
-    def set_sex(self) -> None:
-        """ Set new sex
-        :param sex: new sex as 'm' or 'f'
+    def set_sex(self, sex: Optional[str]) -> None:
+        """ Set new or reset sex
+        :param sex: new sex as 'm' or 'f' or None to reset
         """
-        if self.male_button.GetValue():
-            sex = "MALE"
-        elif self.female_button.GetValue():
-            sex = "FEMALE"
-        return None
+        pass  # TODO
 
     def get_size(self) -> float:
         """ get current size
         :return: current size in meters
         """
-        return self.size
+        pass  # TODO
 
-    def set_size(self) -> None:
+    def set_size(self, size: float) -> None:
         """ Set new size in meters
         :param size: new size
         """
-        self.size = self.input_size.GetValue()
+        pass  # TODO
 
     def get_weight(self) -> float:
         """ get current weight
         :return: current weight in kg
         """
-        pass # TODO
+        pass  # TODO
 
     def set_weight(self, weight: float) -> None:
         """ Set new weight in meters
         :param weight: new weight
         """
-        self.weight = self.input_weight.GetValue()
+        pass  # TODO
 
     def get_ideal_weight(self) -> float:
         """ calculate ideal weight
@@ -74,12 +69,25 @@ class BMIcalc(Panel):
         """
         pass  # TODO
 
+
+class BMIprocessing(Panel):
+    def get_inputs(self):
+        size = self.input_size.GetValue()
+        weight = self.input_weight.GetValue()
+        age = self.input_age.GetValue()
+
+    def click_calc( self, event,BMI,Ideal_weight,result ):#Fehler
+        self.output_bewertung.SetLabelMarkup(result)
+        self.output_BMI.SetLabelMarkup(BMI)
+        self.output_idealgewicht.SetLabelMarkup(Ideal_weight)
+
     def click_exit(self, event):
         self.Parent.Destroy()
+        print(self.input_size.GetValue())
 
 
 app = wx.App()
 frm = wx.Frame(None, title="BMI Rechner", size=wx.Size(350, 270))
-pln = BMIcalc(frm)
+pln = BMIprocessing(frm)
 frm.Show()
 app.MainLoop()
