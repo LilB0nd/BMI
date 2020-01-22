927984556c350905d0ab31ca59d1df0fa90a4c11
joshua121 <joshua121@schule.bremen.de>
2020-01-22 14:22:30 +0100

    plicht: -size -weight
    optinal
    -age
    -sex

--- BMI_programm.py
+++ BMI_programm.py
@@ -1,26 +1,61 @@
 from noname import Panel
 import wx
-#from types import Optional
 
-#class BMIclac(Panel):
-    #def set_table(self):
-     #   self.BMItable = (("Untergewicht", ">18.4"), ("Normalgewicht", 18.5, 24.9), ("Übergewicht", 25, 29.9),
-      #                   ("Starkes Übergewicht(Apdipositas Grad I)", 30, 34.9),("Apdipositas Grad II", 35, 39.9),
-       #                  ("Adipositas Grad III", ">40"))
-        #if self.BMItable[0]:
-         #   print(self.BMItable)
-#
-    #def set_bmi(#self):
-        #pass
+class BMIcalculation:
+    def __init__(self, user_input):
+        self.sex = "male"
+        self.weight = user_input[1]
+        self.size = user_input[0]
+        self.age = user_input[2]
+        self.BMItable = {"no_sex": (("FEHLER", None), ("Untergewicht", 0.0, 18.4), ("Normalgewicht", 18.5, 24.9),
+                                    ("Übergewicht", 25.0, 29.9),
+                                    ("Starkes Übergewicht(Apdipositas Grad I)", 30.0, 34.9),
+                                    ("Apdipositas Grad II", 35.0, 39.9),("Adipositas Grad III", 40.0, 200.0)),
+                         "male": (("FEHLER", None), ("Untergewicht", 0.0, 20.0), ("Normalgewicht", 20.0, 24.9),
+                                  ("Übergewicht", 25.0, 29.9), ("Starkes Übergewicht(Apdipositas Grad I)", 30.0, 34.9),
+                                  ("Apdipositas Grad II", 35.0, 39.9), ("Adipositas Grad III", 40.0, 200.0)),
+                         "female": (("FEHLER", None), ("Untergewicht", 0.0, 19.0), ("Normalgewicht", 19.0, 23.9),
+                                    ("Übergewicht", 24.0, 29.9),
+                                    ("Starkes Übergewicht(Apdipositas Grad I)", 30.0, 34.9),
+                                    ("Apdipositas Grad II", 35.0, 39.9), ("Adipositas Grad III", 40.0, 200.0))}
+        self.idealweigt = ((19, 24, 19, 24), (25, 34, 20, 25), (35, 44, 21, 26), (45, 54, 23, 28), (65, 130, 24, 29))
+
+    def set_ideal(self):
+        for element in self.idealweigt:
+            if self.age > element[0]
+
+    def set_result(self):
+        counter = 1
+        result = 0
+        bmitable = self.BMItable["no_sex"]
+        if self.sex == "male":
+            bmitable = self.BMItable["male"]
+        if self.sex == "female":
+            bmitable = self.BMItable["female"]
+        for element in bmitable[1:]:
+            if self.bmi > element[1] < element[2]:
+                result = counter
+            counter = counter + 1
+        print(bmitable[result][0])
+
+    def get_result(self):
+        pass
+
+    def set_bmi(self):
+        self.bmi = round(self.weight/(self.size**2), 1)
+
+    def get_bmi(self):
+        pass
 
 class BMIprocessing(Panel):
     def get_inputs(self):
+        # TODO sEX input
         size = self.input_size.GetValue()
         weight = self.input_weight.GetValue()
         age = self.input_age.GetValue()
-        return [size,weight,age]
+        return [size, weight, age]
 
-    def click_calc( self, event):
+    def click_calc(self, event):
         self.output_bewertung.SetLabelMarkup("Zu Fett")
         self.output_BMI.SetLabelMarkup("over 9000")
         self.output_idealgewicht.SetLabelMarkup("75")
@@ -29,11 +64,13 @@ class BMIprocessing(Panel):
         self.Parent.Destroy()
 
 
-
 app = wx.App()
 frm = wx.Frame(None, title="BMI Rechner", size=wx.Size(350, 270),
                style=wx.DEFAULT_FRAME_STYLE & ~(wx.RESIZE_BORDER | wx.MAXIMIZE_BOX))
 pln = BMIprocessing(frm)
-#BMIclac.set_bmi()
+input = BMIprocessing.get_inputs(pln)
+BMIcalc = BMIcalculation(input)
+
+
 frm.Show()
 app.MainLoop()
--- testing_calc.py
+++ testing_calc.py
@@ -1,19 +1,19 @@
 class BMIclac:
-    def __init__(self, bmi, sex):
-        self.bmi = bmi
+    def __init__(self, sex, weight, size):
         self.sex = sex
-
-        self.BMItable = {"no_sex": ("FEHLER", ("Untergewicht", 0.0, 18.4), ("Normalgewicht", 18.5, 24.9),
+        self.weight = weight
+        self.size = size
+        self.BMItable = {"no_sex": (("FEHLER", None), ("Untergewicht", 0.0, 18.4), ("Normalgewicht", 18.5, 24.9),
                                     ("Übergewicht", 25.0, 29.9),
                                     ("Starkes Übergewicht(Apdipositas Grad I)", 30.0, 34.9),
-                                    ("Apdipositas Grad II", 35.0, 39.9),("Adipositas Grad III", 40.0, 1000000000000.0)),
-                         "male": ("FEHLER", ("Untergewicht", 0.0, 20.0), ("Normalgewicht", 20.0, 24.9),
+                                    ("Apdipositas Grad II", 35.0, 39.9),("Adipositas Grad III", 40.0, 200.0)),
+                         "male": (("FEHLER", None), ("Untergewicht", 0.0, 20.0), ("Normalgewicht", 20.0, 24.9),
                                   ("Übergewicht", 25.0, 29.9), ("Starkes Übergewicht(Apdipositas Grad I)", 30.0, 34.9),
-                                  ("Apdipositas Grad II", 35.0, 39.9), ("Adipositas Grad III", 40.0, 10000000000000.0)),
-                         "female": ("FEHLER", ("Untergewicht", 0.0, 19.0), ("Normalgewicht", 19.0, 23.9),
+                                  ("Apdipositas Grad II", 35.0, 39.9), ("Adipositas Grad III", 40.0, 200.0)),
+                         "female": (("FEHLER", None), ("Untergewicht", 0.0, 19.0), ("Normalgewicht", 19.0, 23.9),
                                     ("Übergewicht", 24.0, 29.9),
                                     ("Starkes Übergewicht(Apdipositas Grad I)", 30.0, 34.9),
-                                    ("Apdipositas Grad II", 35.0, 39.9), ("Adipositas Grad III", 40.0, 100000000000.0))}
+                                    ("Apdipositas Grad II", 35.0, 39.9), ("Adipositas Grad III", 40.0, 200.0))}
 
     def set_result(self):
         counter = 1
@@ -24,17 +24,20 @@ class BMIclac:
         if self.sex == "female":
             bmitable = self.BMItable["female"]
         for element in bmitable[1:]:
-            if self.bmi > element[1] < element[2]:
+            if self.bmi > element[1] and self.bmi < element[2]:
                 result = counter
             counter = counter + 1
         print(bmitable[result][0])
 
     def set_bmi(self):
-        pass
+        self.bmi = round(self.weight/(self.size**2), 1)
+        print(self.bmi)
 
 
-bmi = float(input("BMI: "))
 sex = str(input("Sex: "))
-BMI = BMIclac(bmi, sex)
+weight = float(input("Weight in kg: "))
+size = ((float(input("Size in cm: "))) / 100)
 
+BMI = BMIclac(sex, weight, size)
+BMI.set_bmi()
 BMI.set_result()
