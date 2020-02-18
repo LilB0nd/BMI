c5045d1b942f69631d3531992c1fcf585587763f
joshua121 <joshua121@schule.bremen.de>
2020-02-18 10:32:37 +0100

    plicht: -size -weight
    optinal
    -age
    -sex

--- BMI_programm.py
+++ BMI_programm.py
@@ -1,22 +1,19 @@
 from noname import Panel
 import wx
+from typing import Optional
 
 
 class BMIcalculation:
-    def __init__(self, user_input):
-        self.sex = "male"
-        self.weight = int(user_input[1])
-        self.size = int(user_input[0])
-        self.age = int(user_input[2])
+
+    def __init__(self):
         self.idealweight = None
         self.idealweight_low = None
         self.idealweight_high = None
         self.bmi = None
         self.result = None
-        self.BMItable = {"no_sex": (("FEHLER", None), ("Untergewicht", 0.0, 18.4), ("Normalgewicht", 18.5, 24.9),
-                                    ("Übergewicht", 25.0, 29.9),
-                                    ("Starkes Übergewicht(Apdipositas Grad I)", 30.0, 34.9),
-                                    ("Apdipositas Grad II", 35.0, 39.9),("Adipositas Grad III", 40.0, 200.0)),
+        self.BMItable = {None: (("FEHLER", None), ("Untergewicht", 0.0, 18.4), ("Normalgewicht", 18.5, 24.9),
+                                ("Übergewicht", 25.0, 29.9), ("Starkes Übergewicht(Apdipositas Grad I)", 30.0, 34.9),
+                                ("Apdipositas Grad II", 35.0, 39.9),("Adipositas Grad III", 40.0, 200.0)),
                          "male": (("FEHLER", None), ("Untergewicht", 0.0, 20.0), ("Normalgewicht", 20.0, 24.9),
                                   ("Übergewicht", 25.0, 29.9), ("Starkes Übergewicht(Apdipositas Grad I)", 30.0, 34.9),
                                   ("Apdipositas Grad II", 35.0, 39.9), ("Adipositas Grad III", 40.0, 200.0)),
@@ -24,19 +21,47 @@ class BMIcalculation:
                                     ("Übergewicht", 24.0, 29.9),
                                     ("Starkes Übergewicht(Apdipositas Grad I)", 30.0, 34.9),
                                     ("Apdipositas Grad II", 35.0, 39.9), ("Adipositas Grad III", 40.0, 200.0))}
-        self.idealweight_table = ((19, 24, 19, 24), (25, 34, 20, 25), (35, 44, 21, 26), (45, 54, 23, 28), (65, 130, 24, 29))
+        self.idealweight_table = ((19, 24, 19, 24), (25, 34, 20, 25), (35, 44, 21, 26), (45, 54, 23, 28),
+                                  (65, 130, 24, 29))
 
-    def set_ideal(self):
-        for element in self.idealweight_table:
-            if self.age >= element[0] and self.age <= element[1]:
-                self.idealweight_low = self.size**2 * element[2]
-                self.idealweight_high = self.size**2 * element[3]
-                self.idealweight = round((self.idealweight_low + self.idealweight_high)/2, 1)
+    def set_size(self, size: float) -> None:
+        self.size = size
+        print(size)
+        return None
 
-    def get_ideal(self):
-        return self.idealweight
+    def get_size(self) -> float:
+        return self.size
 
-    def set_result(self):
+    def set_age(self, age: Optional[int]) -> None:
+        self.age = age
+        print(age)
+        return None
+
+    def get_age(self) -> int:
+        return self.age
+
+    def set_weight(self, weight: float) -> None:
+        self.weight = weight
+        return None
+
+    def get_weight(self) -> float:
+        return self.weight
+
+    def set_sex(self, sex: Optional[str]) -> None:
+        self.sex = sex
+        return None
+
+    def get_sex(self) -> str:
+        return self.sex
+
+    def set_bmi(self) -> None:
+        self.bmi = round(self.weight/(self.size**2), 1)
+        return None
+
+    def get_bmi(self) -> float:
+        return self.bmi
+
+    def set_category(self) -> None:
         counter = 1
         result = 0
         bmitable = self.BMItable["no_sex"]
@@ -49,87 +74,81 @@ class BMIcalculation:
                 result = counter
             counter = counter + 1
         self.result = bmitable[result][0]
+        return None
 
-    def get_result(self):
+    def get_category(self) -> str:
         return self.result
 
-    def set_bmi(self):
-        self.bmi = round(self.weight/(self.size**2), 1)
+    def set_ideal(self) -> None:
+        for element in self.idealweight_table:
+            if self.age >= element[0] and self.age <= element[1]:
+                self.idealweight_low = self.size**2 * element[2]
+                self.idealweight_high = self.size**2 * element[3]
+                self.idealweight = round((self.idealweight_low + self.idealweight_high)/2, 1)
+        return None
 
-    def get_bmi(self):
-        return self.bmi
+    def get_ideal(self) -> float:
+        return self.idealweight
 
 
 class BMIprocessing(Panel):
-    def super__init__(self):
-        self.size = None
-        self.weight = None
-        self.age = None
 
     def get_inputs(self):
         return [self.weight, self.size, self.age]
 
     def set_output(self):
-
-        self.raiting = info[0]
-        self.BMI = info[1]
-        self.idealweight = info[2]
+        self.category = self.BMIcalc.get_category()
+        self.BMI = self.BMIcalc.get_bmi()
+        self.idealweight = self.BMIcalc.get_ideal()
 
     def click_calc(self, event):
-        self.set_output()
-        self.output_raiting.SetLabelMarkup(self.raiting)
+        self.BMIcalc = BMIcalculation()
+        self.BMIcalc.get_bmi()
+        print(self.category)
+        self.output_raiting.SetLabelMarkup(self.category)
         self.output_BMI.SetLabelMarkup(self.BMI)
         self.output_idealweight.SetLabelMarkup( self.idealweight)
 
     def click_exit(self, event):
-        self.Parent.Destroy()
+        self.Destroy()
         exit()
 
     def on_size_input(self, event):
     # Zurück zu schwarz macen
+        self.input_size.SetForegroundColour(wx.BLACK)
         try:
-            self.size = float(self.input_size.GetValue())
-            self.input_size.SetForegroundColour(wx.BLACK)
+            input_size = float(self.input_size.GetValue())
+            BMIcalc.set_size(size = input_size)
         except ValueError:
-            try:
-                self.size = float(self.input_size.GetValue())
-                self.input_size.SetForegroundColour(wx.BLACK)
-            except ValueError:
-                self.input_size.SetForegroundColour(wx.RED)  # set text color
-
+            self.input_size.SetForegroundColour(wx.RED)
+              # set text color
+        event.Skip()
 
     def on_weight_input(self, event):
-        # Zurück zu schwarz macen
+        # Zurück zu schwarz machen
         try:
-            self.weight = int(self.input_weight.GetValue())
             self.input_weight.SetForegroundColour(wx.BLACK)
+            self.weight = int(self.input_weight.GetValue())
         except ValueError:
             self.input_weight.SetForegroundColour(wx.RED)
 
     def on_age_input(self, event):
-        # Zurück zu schwarz macen
+        # Zurück zu schwarz machen
         try:
-            self.input_size.SetForegroundColour(wx.BLACK)
-            self.size = int(self.input_size.GetValue())
+            self.input_age.SetForegroundColour(wx.BLACK)
+            self.age = int(self.input_age.GetValue())
 
         except ValueError:
-            self.input_size.SetForegroundColour(wx.RED)
+            self.input_age.SetForegroundColour(wx.RED)
 
 
 app = wx.App()
-frm = wx.Frame(None, title="BMI Rechner", size=wx.Size(350, 270),
+frm = wx.Frame(None, title="BMI Rechner", size=wx.Size(360, 270),
                style=wx.DEFAULT_FRAME_STYLE & ~(wx.RESIZE_BORDER | wx.MAXIMIZE_BOX))
 pln = BMIprocessing(frm)
+BMIcalc = BMIcalculation()
 frm.Show()
 app.MainLoop()
-
-input = BMIprocessing.get_inputs(pln)
-BMIcalc = BMIcalculation(input)
-BMIcalc.set_bmi()
-BMIcalc.set_result()
-BMIcalc.set_ideal()
-pln.set_output()
-info = [BMIcalc.get_result(), BMIcalc.get_bmi(), BMIcalc.get_ideal()]
 
 
 
--- noname.py
+++ noname.py
@@ -16,11 +16,11 @@ import wx.xrc
 
 class Panel ( wx.Panel ):
 
-	def __init__( self, parent, id = wx.ID_ANY, pos = wx.DefaultPosition, size = wx.Size( 340,240 ), style = wx.TAB_TRAVERSAL, name = wx.EmptyString ):
+	def __init__( self, parent, id = wx.ID_ANY, pos = wx.DefaultPosition, size = wx.Size( 350,240 ), style = wx.TAB_TRAVERSAL, name = wx.EmptyString ):
 		wx.Panel.__init__ ( self, parent, id = id, pos = pos, size = size, style = style, name = name )
 
-		self.SetMinSize( wx.Size( 340,240 ) )
-		self.SetMaxSize( wx.Size( 340,240 ) )
+		self.SetMinSize( wx.Size( 350,240 ) )
+		self.SetMaxSize( wx.Size( 350,240 ) )
 
 		main_sizer = wx.BoxSizer( wx.VERTICAL )
 
@@ -34,8 +34,8 @@ class Panel ( wx.Panel ):
 		self.female_button = wx.RadioButton( Geschlecht.GetStaticBox(), wx.ID_ANY, u"weiblich", wx.DefaultPosition, wx.DefaultSize, 0 )
 		Geschlecht.Add( self.female_button, 1, wx.ALL, 5 )
 
-		self.divers_button = wx.RadioButton( Geschlecht.GetStaticBox(), wx.ID_ANY, u"divers", wx.DefaultPosition, wx.DefaultSize, 0 )
-		Geschlecht.Add( self.divers_button, 0, wx.ALL, 5 )
+		self.no_sex_button = wx.RadioButton( Geschlecht.GetStaticBox(), wx.ID_ANY, u"keine Angabe", wx.DefaultPosition, wx.DefaultSize, 0 )
+		Geschlecht.Add( self.no_sex_button, 1, wx.ALL, 5 )
 
 
 		input_sizer.Add( Geschlecht, 0, wx.EXPAND, 5 )
@@ -68,7 +68,7 @@ class Panel ( wx.Panel ):
 		gewicht_sizer.Add( self.input_size, 1, wx.ALL|wx.ALIGN_CENTER_VERTICAL, 5 )
 
 		self.input_weight = wx.TextCtrl( Personen_sizer.GetStaticBox(), wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, wx.TE_CENTER )
-		gewicht_sizer.Add( self.input_weight, 1, wx.ALL|wx.ALIGN_CENTER_VERTICAL, 5 )
+		gewicht_sizer.Add( self.input_weight, 0, wx.ALL, 5 )
 
 		self.input_age = wx.TextCtrl( Personen_sizer.GetStaticBox(), wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, wx.TE_CENTER )
 		gewicht_sizer.Add( self.input_age, 1, wx.ALL, 5 )
@@ -182,7 +182,7 @@ class Panel ( wx.Panel ):
 		self.Layout()
 
 		# Connect Events
-		self.input_size.Bind( wx.EVT_TEXT, self.on_size_input )
+		self.input_size.Bind( wx.EVT_KEY_UP, self.on_size_input )
 		self.input_weight.Bind( wx.EVT_TEXT, self.on_weight_input )
 		self.input_age.Bind( wx.EVT_TEXT, self.on_age_input )
 		self.button_exit.Bind( wx.EVT_BUTTON, self.click_exit )
