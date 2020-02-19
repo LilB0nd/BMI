894991b482f931ce3199ed02a340b1d310270126
joshua121 <Joshua121@schule.bremen.de>
2020-02-19 13:35:22 +0100

    Geschlecht Knöpfe in BMIprossecing Die Möglichkeit geben alter nicht anzugeben

--- BMI_programm.py
+++ BMI_programm.py
@@ -4,19 +4,6 @@ from typing import Optional
 
 
 class BMIcalculation:
-    def __init__(self):
-        self.BMItable = {None: (("FEHLER", None), ("Untergewicht", 0.0, 18.4), ("Normalgewicht", 18.5, 24.9),
-                                ("Übergewicht", 25.0, 29.9), ("Starkes Übergewicht(Apdipositas Grad I)", 30.0, 34.9),
-                                ("Apdipositas Grad II", 35.0, 39.9), ("Adipositas Grad III", 40.0, 200.0)),
-                         "male": (("FEHLER", None), ("Untergewicht", 0.0, 20.0), ("Normalgewicht", 20.0, 24.9),
-                                  ("Übergewicht", 25.0, 29.9), ("Starkes Übergewicht(Apdipositas Grad I)", 30.0, 34.9),
-                                  ("Apdipositas Grad II", 35.0, 39.9), ("Adipositas Grad III", 40.0, 200.0)),
-                         "female": (("FEHLER", None), ("Untergewicht", 0.0, 19.0), ("Normalgewicht", 19.0, 23.9),
-                                    ("Übergewicht", 24.0, 29.9),
-                                    ("Starkes Übergewicht(Apdipositas Grad I)", 30.0, 34.9),
-                                    ("Apdipositas Grad II", 35.0, 39.9), ("Adipositas Grad III", 40.0, 200.0))}
-
-
     def set_size(self, size: float) -> None:
         self.size = size/100
         return None
@@ -149,7 +136,3 @@ pln = BMIprocessing(frm)
 pln.my_init_(BMIcalc)
 frm.Show()
 app.MainLoop()
-
-
-
-
